f1b62fe4e85e82d1a09cfa71dddac59f12339c78
João Oliveira <jaoovit@gmail.com>
2025-12-15 20:06:45 +0000

    Create email repository

---
+++ email/src/main/java/com/oliveira/email/repository/EmailRepository.java
@@ -0,0 +1,9 @@
+package com.oliveira.email.repository;
+
+import com.oliveira.email.model.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<Email, UUID> {
+}
