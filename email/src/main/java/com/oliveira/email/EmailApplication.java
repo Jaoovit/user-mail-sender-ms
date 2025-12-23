ab4aca8496ccb5807b9f6a5ed763621aaf01bbb8
João Oliveira <jaoovit@gmail.com>
2025-12-23 16:24:38 +0000

    Refactory .env

--- email/src/main/java/com/oliveira/email/EmailApplication.java
+++ email/src/main/java/com/oliveira/email/EmailApplication.java
@@ -2,12 +2,12 @@ package com.oliveira.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mail.autoconfigure.MailSenderAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MailSenderAutoConfiguration.class })
 public class EmailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
 	}
-
 }
